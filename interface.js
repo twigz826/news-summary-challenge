3081b76ee35e4095f93f726fc013860df4b3be5b
twigz826 <thomastwigden@gmail.com>
2021-02-21 18:16:06 -0300

    Added article.js

--- interface.js
+++ interface.js
@@ -1,5 +1,6 @@
 const url = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?order-by=newest";
 let news = new News();
+let myImage = "/Users/ttwigden/Desktop/Makers/Projects/week-7/news-summary-challenge/images/Peter_Griffin.png"
 
 function getData() {
   return fetch(url).then(response => {
@@ -9,5 +10,20 @@ function getData() {
 
 getData().then(post => {
   let newsData = post.response.results
-  printTitles(newsData)
+  saveAll(newsData)
 });
+
+function saveAll(articles) {
+  articles.forEach(article => {
+    let headline = article.webTitle
+    let image = myImage
+    let story = "No story available"
+    let url = article.webUrl
+    // if (article.fields)
+    //   image = article.fields.thumbnail
+    //   story = article.fields.body
+    let newsArticle = new Article(headline, image, story, url)
+    news.add(newsArticle)
+  })
+  displayAll()
+}
