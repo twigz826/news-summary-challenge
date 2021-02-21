90e21c3cd0ebfc07cdd0789169b9485c61946ea4
twigz826 <thomastwigden@gmail.com>
2021-02-21 17:31:02 -0300

    Updated interface.js

--- interface.js
+++ interface.js
@@ -1,11 +1,13 @@
 const url = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?order-by=newest";
-let out = "";
-let articles = []
-
-getData();
+let news = new News();
 
 function getData() {
-  return fetch(url).then((response) => {
-    return response.json()
+  return fetch(url).then(response => {
+    return response.json();
   });
 }
+
+getData().then(post => {
+  let newsData = post.response.results
+  printTitles(newsData)
+});
