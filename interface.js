c0f94b921bf0513619195dfe4c55bbaecc285405
twigz826 <thomastwigden@gmail.com>
2021-02-21 21:42:44 -0300

    Struggling with image and story still

--- interface.js
+++ interface.js
@@ -1,9 +1,18 @@
+// Variables
 let url = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?order-by=newest";
 let news = new News();
 let myImage = "/Users/ttwigden/Desktop/Makers/Projects/week-7/news-summary-challenge/images/Peter_Griffin.png"
+let modal = document.getElementById("articleModal");
+let span = document.getElementById("close");
 
+// Event listeners
+span.addEventListener('click', closeModal, false)
+window.addEventListener('hashchange', openModal, false)
+
+// Functions
 function getData() {
   return fetch(url).then(response => {
+    console.log(response.json());
     return response.json();
   });
 }
@@ -17,12 +26,12 @@ function saveAll(articles) {
   articles.forEach(article => {
     let headline = article.webTitle;
     let image = myImage;
-    let story = "No story available";
+    let story = "No story";
     let url = article.webUrl;
     console.log(article.fields);
-    if (article.fields)
-      image = article.fields.thumbnail
-      story = article.fields.body
+    // if (article.fields)
+    //   image = article.fields.thumbnail
+    //   story = article.fields.body
     let newsArticle = new Article(headline, image, story, url);
     news.add(newsArticle)
   });
@@ -39,4 +48,21 @@ function displayAll() {
     headlineDiv.insertAdjacentHTML('beforeend', `<h3><a href=#${index}>${article.getHeadline()}</a></h3>`)
     headlinesDiv.appendChild(headlineDiv)
   })
+}
+
+function openModal() {
+  modal.style.display = "block";
+  let articles = news.getArticles()
+  let index = location.href.split('#')[1]
+  console.log(index);
+  let selectedArticle = articles[index]
+  let selectedArticleDiv = document.getElementById("selectedArticle")
+  console.log(selectedArticle)
+  selectedArticleDiv.insertAdjacentHTML('beforeend', `<h1><a href='${selectedArticle.getArticleURL()}'>${selectedArticle.getHeadline()}</a></h1>`)
+  selectedArticleDiv.insertAdjacentHTML('beforeend', `${selectedArticle.getStory()}`)
+}
+
+function closeModal() {
+  modal.style.display = "none";
+  // reset to no hash?
 }
